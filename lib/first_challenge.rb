0383e421c0b8c17a5af6e698f4bce38c025d7929
Susan Tan <ssusantan9@gmail.com>
2019-03-26 19:23:22 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,22 +1,28 @@
-def first_challenge
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
+contacts = {
+  'Jon Snow' => {
+    name: 'Jon',
+    email: 'jon_snow@thewall.we',
+    favorite_icecream_flavors: ['chocolate', 'vanilla', 'mint chip'],
+    knows: nil
+  },
+  'Freddy Mercury' => {
+    name: 'Freddy',
+    email: 'freddy@mercury.com',
+    favorite_icecream_flavors: ['strawberry', 'cookie dough', 'mint chip']
   }
+}
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+def remove(contacts)
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if {|flavor| flavor == "strawberry"}   
+      puts "#{value}"
+    end
+  end
 end
+contacts["Freddy Mercury"][:favorite_icecream_flavors]
+end
+
+remove(contacts)
 
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,4 +1,3 @@
-def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
    vegetable: ["carrots", "broccoli", "cucumbers"],
@@ -6,8 +5,8 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  #code your solution here!
+  def flatten(groceries)
+  groceries.values.flatten
+  end
 
-  
-
-end
+  flatten(groceries)
