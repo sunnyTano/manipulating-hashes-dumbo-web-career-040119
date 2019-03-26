67c3debc80d92d8c408741fd9dd4329f19739d87
Susan Tan <ssusantan9@gmail.com>
2019-03-26 19:24:19 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,16 @@
-contacts = {
-  'Jon Snow' => {
-    name: 'Jon',
-    email: 'jon_snow@thewall.we',
-    favorite_icecream_flavors: ['chocolate', 'vanilla', 'mint chip'],
-    knows: nil
-  },
-  'Freddy Mercury' => {
-    name: 'Freddy',
-    email: 'freddy@mercury.com',
-    favorite_icecream_flavors: ['strawberry', 'cookie dough', 'mint chip']
-  }
-}
+# contacts = {
+#   'Jon Snow' => {
+#     name: 'Jon',
+#     email: 'jon_snow@thewall.we',
+#     favorite_icecream_flavors: ['chocolate', 'vanilla', 'mint chip'],
+#     knows: nil
+#   },
+#   'Freddy Mercury' => {
+#     name: 'Freddy',
+#     email: 'freddy@mercury.com',
+#     favorite_icecream_flavors: ['strawberry', 'cookie dough', 'mint chip']
+#   }
+# }
 
 def remove(contacts)
 contacts.each do |person, data|
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
-  groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
-  }
+  # groceries = {
+  # dairy: ["milk", "yogurt", "cheese"],
+  # vegetable: ["carrots", "broccoli", "cucumbers"],
+  # meat: ["chicken", "steak", "salmon"],
+  # grains: ["rice", "pasta"]
+  # }
 
   def flatten(groceries)
   groceries.values.flatten
